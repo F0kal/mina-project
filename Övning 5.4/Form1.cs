deb41dbd5ff70a00e6e1e9227141e3fdcacbf87c
David Eriksson <david.eriksson.edu@rattvik.se>
2023-11-07 12:07:25 +0100

    xdd

--- Övning 5.4/Form1.cs
+++ Övning 5.4/Form1.cs
@@ -20,13 +20,13 @@ namespace Övning_5._4
 
         private void btnKör_Click(object sender, EventArgs e)
         {
-            int talA = int.Parse(textBox1.Text);
-            int talB = int.Parse(textBox2.Text);
-            int talC = int.Parse(textBox3.Text);
+            int talA = int.Parse(textBox1.Text);           
+            int talB = int.Parse(textBox2.Text);            
+            int talC = int.Parse(textBox3.Text);          
 
-            bool svar = (talC == talB + 1 && talB == talA + 1) || (talC == talB - 1 && talB == talA - 1);
+            bool svar = (talC == talB + 1 && talB == talA + 1) || (talC == talB - 1 && talB == talA - 1); // den jämnför talen för att se om dem ligger i ordning
 
-            if (svar) { lblSvar.Text = " talen är i ordningsföljd"; }
+            if (svar) { lblSvar.Text = " talen är i ordningsföljd"; } 
 
             else { lblSvar.Text = " talen är inte i ordningsföljd"; }
            
